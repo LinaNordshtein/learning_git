d2a6c5adcfb23840a68e8b8fe376ca28a8f7e910
Linas <linanord90@gmail.com>
2021-07-28 13:35:54 +0300

    realise

--- 2.js
+++ 2.js
@@ -1,4 +1,16 @@
-CREATE DATABASE university;
-USE university;
+
+
+
+
+
+
+
+
+
+
+
+
+cREATE DATABASE university;
+USE universityiiiiiiiI;
 SOURCE <path_of_DLL.sql_file>;
 SOURCE <path_of_InsertStatements.sql_file>;
